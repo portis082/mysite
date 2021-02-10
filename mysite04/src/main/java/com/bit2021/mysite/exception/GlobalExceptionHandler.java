6b522adca00c037cbdf4909019bae3911771a9e5
MS <MS@DESKTOP-VEVTDE0>
2021-02-10 21:18:56 +0900

    fix

--- mysite04/src/main/java/com/bit2021/mysite/exception/GlobalExceptionHandler.java
+++ mysite04/src/main/java/com/bit2021/mysite/exception/GlobalExceptionHandler.java
@@ -3,16 +3,19 @@ package com.bit2021.mysite.exception;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 	
+	private static final Log LOG = LogFactory.getLog(GlobalExceptionHandler.class);
 	@ExceptionHandler(Exception.class)
 	public void handlerException(HttpServletRequest request, HttpServletResponse response, Exception e) throws Exception {
 		// 로깅작업
-		System.out.println("log : " + e);
+		LOG.error("log : " + e);
 		
 		// 안내 페이지
 		request.setAttribute("exception", e.toString());
